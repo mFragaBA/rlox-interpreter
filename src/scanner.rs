62c2944744ca5aa407b127ff3d37f50407965465
Martin Fraga <martin.fraga@lambdaclass.com>
2022-07-31 19:57:57 -0300

    stop using peekable and charsIndex

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,4 +1,4 @@
-use std::str::CharIndices;
+use std::str::Chars;
 use std::iter::Peekable;
 
 use crate::{TokenType, LoxResult, errors, keywords};
@@ -9,19 +9,20 @@ pub struct Scanner<'a> {
     source: &'a str,
     tokens: Vec<Token<'a>>,
     start: usize,
-    current: Peekable<CharIndices<'a>>,
+    at: usize,
+    current: Chars<'a>,
     line: usize,
     failed: bool,
 }
 
 impl<'a> Scanner<'a> {
     pub fn new(source: &'a str) -> Scanner<'a> {
-        let mut chars = source.char_indices().peekable();
         Scanner {
             source,
             tokens: Vec::new(),
-            start: chars.peek().map(|(index, _c)| *index).unwrap_or(0),
-            current: chars,
+            start: 0,
+            at: 0,
+            current: source.chars(),
             line: 1,
             failed: false,
         }
@@ -39,52 +40,52 @@ impl<'a> Scanner<'a> {
         Ok(&self.tokens)
     }
 
-    fn is_at_end(&mut self) -> bool {
-        self.current.peek().is_none()
+    fn is_at_end(&mut self) -> bool { 
+        self.peek_first_char().is_none()
     }
 
     fn scan_token(&mut self) {
-        let (i, c) = self.current.next().expect("called scan_token with no remaining chars");
+        let c = self.advance(); 
         match c {
-            '(' => self.add_token(i, TokenType::LEFT_PAREN, None),
-            ')' => self.add_token(i, TokenType::RIGHT_PAREN, None),
-            '{' => self.add_token(i, TokenType::LEFT_BRACE, None),
-            '}' => self.add_token(i, TokenType::RIGHT_BRACE, None),
-            ',' => self.add_token(i, TokenType::COMMA, None),
-            '.' => self.add_token(i, TokenType::DOT, None),
-            '-' => self.add_token(i, TokenType::MINUS, None),
-            '+' => self.add_token(i, TokenType::PLUS, None),
-            ';' => self.add_token(i, TokenType::SEMICOLON, None),
-            '*' => self.add_token(i, TokenType::STAR, None),
+            '(' => self.add_token(self.at-1, TokenType::LEFT_PAREN, None),
+            ')' => self.add_token(self.at-1, TokenType::RIGHT_PAREN, None),
+            '{' => self.add_token(self.at-1, TokenType::LEFT_BRACE, None),
+            '}' => self.add_token(self.at-1, TokenType::RIGHT_BRACE, None),
+            ',' => self.add_token(self.at-1, TokenType::COMMA, None),
+            '.' => self.add_token(self.at-1, TokenType::DOT, None),
+            '-' => self.add_token(self.at-1, TokenType::MINUS, None),
+            '+' => self.add_token(self.at-1, TokenType::PLUS, None),
+            ';' => self.add_token(self.at-1, TokenType::SEMICOLON, None),
+            '*' => self.add_token(self.at-1, TokenType::STAR, None),
 
             // Operators
             '!' => {
                 if self.match_next_char('=') {
-                    self.add_token(i+1, TokenType::BANG_EQUAL, None);
+                    self.add_token(self.at-1, TokenType::BANG_EQUAL, None);
                 } else {
-                    self.add_token(i, TokenType::BANG, None);
+                    self.add_token(self.at-1, TokenType::BANG, None);
                 }
             }
             '=' => {
                 if self.match_next_char('=') {
-                    self.add_token(i+1, TokenType::EQUAL_EQUAL, None);
+                    self.add_token(self.at-1, TokenType::EQUAL_EQUAL, None);
                 } else {
-                    self.add_token(i, TokenType::EQUAL, None);
+                    self.add_token(self.at-1, TokenType::EQUAL, None);
                 }
 
             }
             '<' => {
                 if self.match_next_char('=') {
-                    self.add_token(i+1, TokenType::LESS_EQUAL, None);
+                    self.add_token(self.at-1, TokenType::LESS_EQUAL, None);
                 } else {
-                    self.add_token(i, TokenType::LESS, None);
+                    self.add_token(self.at-1, TokenType::LESS, None);
                 }
             }
             '>' => {
                 if self.match_next_char('=') {
-                    self.add_token(i+1, TokenType::GREATER_EQUAL, None);
+                    self.add_token(self.at-1, TokenType::GREATER_EQUAL, None);
                 } else {
-                    self.add_token(i, TokenType::GREATER, None);
+                    self.add_token(self.at-1, TokenType::GREATER, None);
                 };
 
             }
@@ -93,16 +94,21 @@ impl<'a> Scanner<'a> {
             '/' => {
                 if self.match_next_char('/') {
                     // A comment goes until the end of the line.
-                    self.start += 1;
+                    self.start += 2;
                     loop {
                         self.start += 1;
-                        match self.current.peek() {
-                            Some((_, '\n')) | None => break,
-                            Some(_) => { self.current.next().expect("Could not advance through source code"); }
+                        self.at += 1;
+                        match self.next_char() {
+                            Some('\n') => {
+                                self.line += 1;
+                                break;
+                            }
+                            Some(_) => { }
+                            None => break,
                         }
                     }
                 } else {
-                    self.add_token(i, TokenType::SLASH, None);
+                    self.add_token(self.at-1, TokenType::SLASH, None);
                 }
             }
             ' ' | '\r' | '\t' => self.start += 1,
@@ -114,18 +120,18 @@ impl<'a> Scanner<'a> {
             // Strings
             '"' => {
                 loop {
-                    match self.current.peek() {
-                        Some((i, '"')) => {
-                            let current_pos = *i;
-                            self.current.next().expect("Could not advance through source code"); 
+                    match self.peek_first_char() {
+                        Some('"') => {
+                            let current_pos = self.at;
+                            self.advance();
 
                             let value = &self.source[self.start+1..=current_pos-1];
                             self.add_token(current_pos, TokenType::STRING, Some(value));
                             break;
                         }
-                        Some((_, c)) => { 
-                            if *c == '\n' { self.line += 1; }
-                            self.current.next().expect("Could not advance through source code"); 
+                        Some(c) => { 
+                            if c == '\n' { self.line += 1; }
+                            self.advance();
                         }
                         None => errors::error(self.line, String::from("Unterminated String")),
                     }
@@ -137,18 +143,18 @@ impl<'a> Scanner<'a> {
             // -123 is not a literal but an expression
             d if d.is_ascii_digit() => {
                 loop {
-                   match self.current.peek() {
-                        Some((_, d)) if d.is_ascii_digit() => { self.current.next().expect("Could not advance through source code"); }
-                        Some((_, '.')) => {
-                            let after_point = self.current.nth(1);
-                            if let Some((_, d)) = after_point {
+                   match self.peek_first_char() {
+                        Some(d) if d.is_ascii_digit() => { self.advance(); }
+                        Some('.') => {
+                            let after_point = self.peek_second_char();
+                            if let Some(d) = after_point {
                                 if d.is_ascii_digit() {
-                                    self.current.next().expect("Could not advance through source code"); 
+                                    self.advance();
                                 }
                             }
                         }
-                        Some((i, _)) => {
-                            let pos = (*i) - 1;
+                        Some(_) => {
+                            let pos = self.at - 1;
                             let num_str = &self.source[self.start..=pos];
                             self.add_token(pos ,TokenType::NUMBER, Some(num_str));
                             break;
@@ -160,10 +166,10 @@ impl<'a> Scanner<'a> {
 
             c if c.is_alphanumeric() => {
                 loop {
-                    match self.current.peek() {
-                        Some((_, c)) if c.is_alphanumeric() => { self.current.next().expect("Could not advance through source code"); }
-                        Some((i, _)) => {
-                            let pos = (*i) - 1;
+                    match self.peek_first_char() {
+                        Some(c) if c.is_alphanumeric() => { self.advance(); }
+                        Some(_) => {
+                            let pos = self.at - 1;
                             let text = &self.source[self.start..=pos];
                             let kind = match keywords::KEYWORDS.get(text) {
                                 Some(kind) => { *kind },
@@ -193,10 +199,29 @@ impl<'a> Scanner<'a> {
         self.tokens.push(token);
     }
 
+    fn advance(&mut self) -> char {
+        self.at += 1;
+        self.next_char().expect("called scan_token with no remaining chars")
+    }
+
+    fn next_char(&mut self) -> Option<char> {
+        self.current.next()
+    }
+
+    fn peek_first_char(&self) -> Option<char> {
+        self.current.clone().next()
+    }
+
+    fn peek_second_char(&self) -> Option<char> {
+        let mut iter_clone = self.current.clone();
+        iter_clone.next();
+        iter_clone.next()
+    }
+
     fn match_next_char(&mut self, expected: char) -> bool {
-        match self.current.peek() {
-            Some((_, c)) if *c == expected => {
-                self.current.next().expect("Could not advance through source code");
+        match self.peek_first_char() {
+            Some(c) if c == expected => {
+                self.advance();
                 true
             }
             _ => false,
